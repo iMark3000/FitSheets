8388f6bafe908c0898ddb930277f790e3120626b
Mark Young <markyoung.ca@gmail.com>
2025-01-05 04:44:47 -0800

    Created runtime args

---
+++ utils/runtime_args.py
@@ -0,0 +1,37 @@
+import argparse
+
+
+def parse_runtime_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Pick which ones")
+    parser.add_argument(
+        "-n", "--nutrition",
+        help="Pulls nutrition data",
+        dest="nutrition",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-a", "--activity",
+        help="Pulls activity data",
+        dest="activity",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-w", "--weight",
+        help="Pulls weight log data",
+        dest="weight",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-s", "--sleep",
+        help="Pulls sleep log data",
+        dest="sleep",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-r", "--refresh-token",
+        help="Refreshes access token",
+        dest="refresh_token",
+        action="store_true"
+    )
+
+    return parser.parse_args()
